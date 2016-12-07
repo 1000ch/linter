574d2804f5eb240cefef42e34920592acdc9b7cb
1000ch <shogo.sensui@gmail.com>
2016-12-07 17:57:09 +0900

    Update

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+'use strict';
+
 const globby = require('globby');
 const eslint = require('eslint');
 const config = require('./config');
 
-exports.lintText = (text, options) => {
+function normalize(argv) {
+  const defaultOptions = {
+    ignore: []
+  };
+
+  const options = Object.assign(defaultOptions, argv);
+
+  options.ignore = options.ignore.concat([
+    '**/node_modules/**',
+    '**/bower_components/**',
+    'coverage/**',
+    '{tmp,temp}/**',
+    '**/*.min.js',
+    '**/bundle.js',
+    '{test,tests,spec,__tests__}/fixture{s,}/**',
+    'vendor/**',
+    'dist/**'
+  ]);
+
+  return options;
+}
+
+exports.lintText = (text, argv) => {
   const engine = new eslint.CLIEngine(config);
-  const fileName = options.fileName;
-	return engine.executeOnText(text, fileName);
+  const options = normalize(argv);
+
+  return engine.executeOnText(text, options.fileName);
 };
 
-exports.lintFiles = (args, options) => {
+exports.lintFiles = (args, argv) => {
   const engine = new eslint.CLIEngine(config);
+  const paths = args.length ? args : '**/*.js';
+  const options = normalize(argv);
 
-  return globby(args).then(paths => engine.executeOnFiles(paths, options));
+  return globby(paths, {
+    ignore: options.ignore
+  }).then(paths => engine.executeOnFiles(paths, options));
 };
 
 exports.getFormatter = eslint.CLIEngine.getFormatter;
